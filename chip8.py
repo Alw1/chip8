3921c0d093e1794f5ff5d542fb972b86ccc9088b
Alex Wyatt <alexander@jdwyatt.com>
2024-08-25 08:22:50 -0400

    First commit

---
+++ chip8.py
@@ -0,0 +1,179 @@
+from enum import Enum
+from time import sleep
+
+class Register(Enum):
+    V0 = 0
+    V1 = 1
+    V2 = 2
+    V3 = 3
+    V4 = 4
+    V5 = 5
+    V6 = 6
+    V7 = 7
+    V8 = 8
+    V9 = 9
+    VA = 10
+    VB = 11
+    VC = 12
+    VD = 13
+    VE = 14
+    VF = 15
+
+
+fontset = [
+	0xF0, 0x90, 0x90, 0x90, 0xF0, 
+	0x20, 0x60, 0x20, 0x20, 0x70, 
+	0xF0, 0x10, 0xF0, 0x80, 0xF0, 
+	0xF0, 0x10, 0xF0, 0x10, 0xF0, 
+	0x90, 0x90, 0xF0, 0x10, 0x10, 
+	0xF0, 0x80, 0xF0, 0x10, 0xF0, 
+	0xF0, 0x80, 0xF0, 0x90, 0xF0, 
+	0xF0, 0x10, 0x20, 0x40, 0x40, 
+	0xF0, 0x90, 0xF0, 0x90, 0xF0, 
+	0xF0, 0x90, 0xF0, 0x10, 0xF0, 
+	0xF0, 0x90, 0xF0, 0x90, 0x90, 
+	0xE0, 0x90, 0xE0, 0x90, 0xE0, 
+	0xF0, 0x80, 0x80, 0x80, 0xF0, 
+	0xE0, 0x90, 0x90, 0x90, 0xE0, 
+	0xF0, 0x80, 0xF0, 0x80, 0xF0, 
+	0xF0, 0x80, 0xF0, 0x80, 0x80  
+]
+
+
+class Display():
+
+    def __init__(self):
+        self.display = [[0] * 32] * 64
+    
+    def clear(self):
+        self.display = [[0] * 32] * 64
+
+    def draw(self, x, y, val):
+        self.display[x][y] = val
+
+
+class Memory():
+
+    FONT_START_ADDRESS = 0x50
+    FONT_END_ADDRESS = 0x50 + len(fontset)
+    ROM_START_ADDRESS = 0x200
+
+    def __init__(self, rom):
+
+        #4096 kb of memory (0x000 - 0x1FF is not used, originally for interpreter)
+        self.memory = [0] * 4096
+
+        #Set fontset into memory
+        self.memory[Memory.FONT_START_ADDRESS : Memory.FONT_END_ADDRESS] = fontset
+
+        #Load ROM into memory
+        self.ROM_END_ADDRESS = self.load_ROM(rom)
+            
+    def load_ROM(self, rom):
+
+        #load ROM into memory byte by byte
+        with open(rom, 'rb') as f:
+            i = 0
+            while True:
+                byte = f.read(1)
+    
+                if not byte:
+                    break
+
+                self.memory[Memory.ROM_START_ADDRESS + i] = byte
+                i += 1
+
+        return Memory.ROM_START_ADDRESS + i
+
+    def debug(self):
+
+        print("ROM DATA")
+        for byte in self.memory[Memory.ROM_START_ADDRESS : self.ROM_END_ADDRESS]:
+            print(byte)
+
+        print("FONT SET")
+        for byte in self.memory[Memory.FONT_START_ADDRESS : Memory.FONT_END_ADDRESS]:
+            print(byte)
+
+
+class CHIP_8():
+
+    def __init__(self, rom):
+
+        #16 8-bit registers
+        self.registers = [0] * 16 
+
+        self.index_register = 0
+
+        #Program Counter
+        self.pc = 0x200
+
+        #4096 kb of memory
+        self.memory = Memory(rom)
+
+        self.memory.debug()
+
+        self.stack = [0] * 16
+
+        self.stack_pointer = 0
+
+        self.keypad = [0] * 16
+        self.sound_timer = 0
+        self.delay_timer = 0
+    
+        #64 x 32 display
+        self.display = Display()
+
+    def setRegister(self, register, num):
+
+        if register is not Register:
+            print(f"ERROR: {register} is not a valid register")
+            exit()
+
+        if 0x00 < num < 0xFF:
+            print(f"ERROR: Number is too big for register")
+            exit()
+
+        self.registers[register] = num
+
+
+    def tick(self):
+        if self.delay_timer != 0:
+            self.delay_timer -= 1
+
+    def fetch_byte(self, addr):
+        return self.memory[addr]
+
+    def fetch_instruction(self, addr):
+        return (self.memory[addr], self.memory[addr+1])
+
+
+    def execute(self):
+
+        """
+            Note for me later,
+            split instructions into top and bottom words
+            then match the case from there to determine what the actuak'
+            instruction is to make it easie rto diiferentiate 
+        """
+        #Upper and lower bytes of the instruction split
+        upper,lower = self.fetch_instruction(self.pc)
+    
+
+        match upper:
+            case 0x00E0:
+                self.display.clear()
+            case 0x00EE:
+                self.pc = self.stack[-1]
+                self.stack_pointer -= 1
+            case _:
+                print(f"ERROR: Invalid instruction {byte}")
+                exit()
+
+
+        pass
+
+    def debug(self):
+        registers = [f"{i}: {val}" for i, val in enumerate(self.registers)]
+        print("Registers")
+        print("\n".join(registers))
---
+++ main.py
@@ -0,0 +1,15 @@
+from chip8 import CHIP_8
+from argparse import ArgumentParser
+
+parser = ArgumentParser(description="Shitty CHIP-8 Emulator")
+parser.add_argument("ROM")
+
+args = parser.parse_args()
+
+if __name__ == '__main__':
+ 
+    chip8 = CHIP_8(args.ROM)
+ 
+    # with open(args.source)
+    # print("FUCK")
+  
